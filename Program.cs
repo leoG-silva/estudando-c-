90886a2580b34d8ddae5549f0854550651af09bf
leoG-silva <leosp94@hotmail.com>
2020-10-13 23:15:54 -0300

    Calculadora com operações

--- Program.cs
+++ Program.cs
@@ -6,12 +6,39 @@ namespace estudando_c_
     {
         static void Main(string[] args)
         {
-            var num1 = "Primeiro";
-            var num2 = "Código";
-            Console.WriteLine(num1 + " " + num2);
-            var resposta = Console.ReadLine();
+            Console.WriteLine("Qual operação deseja realizar?");
+            Console.WriteLine("Soma: '+' | Subtração: '-' | Multiplicação: '*' | Divisão : '/'");
+            var operacao = Console.ReadLine();
 
-            Console.WriteLine("Esse é a minha resposta " + resposta);
+            Console.WriteLine("Agora informe os dois números!");
+            var numero1 = Convert.ToInt32(Console.ReadLine());
+            var numero2 = Convert.ToInt32(Console.ReadLine());
+            var resultado = 0;
+
+            switch (operacao) {
+                 case "+":
+                 resultado = numero1 + numero2; 
+                 break;
+
+                 case "-":
+                 resultado = numero1 - numero2;
+                 break;
+
+                 case "*":
+                 resultado = numero1 * numero2;
+                 break;
+
+                 case "/":
+                 resultado = numero1 / numero2;
+                 break;       
+
+                 default: 
+                 Console.WriteLine("Você não selecionou as operações corretamente!");
+                 break;
+            };
+
+            Console.WriteLine("O resultado da sua operação é: " + resultado);
+            Console.ReadKey();
         }
     }
 }
