a7fb2220d7113e5189ec6bccc1d1270231541f24
leoG-silva <leosp94@hotmail.com>
2020-10-26 16:42:30 -0300

    estudo feira

--- Program.cs
+++ Program.cs
@@ -5,21 +5,36 @@ namespace estudando_c_
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("Olá, Somos da ViTintas");
-            Console.WriteLine("Nesse programa, voce irá descobrir saber a quantidade de tintas e seu valor para pintar a sua residência");
-            Console.WriteLine("Quantos metros quadrados de parede você deseja pintar em sua residência?");
-            var metrosQuadrados = Convert.ToInt32(Console.ReadLine());
+        {
+            Console.WriteLine("Olá, bem vindo a Fruteiras Cangaiba");
+            Console.WriteLine("Quantos Kg de morango você deseja?");
+            var quantidadeMorango = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Agora, qual a quantidade de maça que você deseja?");
+            var quantidadeMaca = Convert.ToDouble(Console.ReadLine());   
 
+            double valorMorango = 0;
+            double valorMaca = 0;
+                     
+            if (quantidadeMorango <= 5) {
+                valorMorango = (quantidadeMorango * 2.50);
+            } else {
+                valorMorango = (quantidadeMorango * 2.20);
+            };
+
+            if (quantidadeMaca <= 5) {
+                valorMaca = (quantidadeMaca * 1.80);
+            } else {
+                valorMaca = (quantidadeMaca * 1.50);
+            };
+
+            var quantidadeTotal = quantidadeMorango + quantidadeMaca;
+            var valorTotal = Math.Round((valorMorango + valorMaca), 2);            
             
-            var lataTinta = 18;
-            var valorTinta = 80;
-            var quantidadeLitros = metrosQuadrados / 3;
-            var quantidadeTinta = quantidadeLitros / lataTinta;
+            if ((quantidadeTotal > 8) || (valorTotal > 25)) {
+                valorTotal = Math.Round(Convert.ToDouble(valorTotal * 0.90), 2);
+            };
 
-            var valorFinal = quantidadeTinta * valorTinta;
-
-            Console.WriteLine($"Você deverá comprar {quantidadeTinta} latas de tinta, sendo o valor total de R${valorFinal}!");
+            Console.WriteLine ($"Você comprou {quantidadeTotal} kg de frutas, totalizando o valor de R${valorTotal}.");
         }
     }                                    
 }
