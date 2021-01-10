c86a1366257155b3e82560dc9b8360457a926709
leoG-silva <leosp94@hotmail.com>
2021-01-10 13:10:30 -0300

    Exercicio orientação ao objetivo

--- Program.cs
+++ Program.cs
@@ -1,177 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//1. Crie uma classe para representar uma pessoa, com os atributos privados de nome, data de nascimento e altura. Crie os métodos públicos para imprimir todos dados de uma pessoa e para calcular a idade da pessoa.
+
+using System;
+using estudando_c_.Modelo;
 
 namespace estudando_c_
 {
-    class Program
+  class Program
+  {
+    private static Pessoa pessoa;
+    private static string nomeUsuario;
+    private static DateTime dataNascimento;
+            
+    static void Main(string[] args)
+    { 
+      pessoa = new Pessoa();
+      Introducao();
+      Menu();       
+      ImprimirDados();
+    }  
+
+    static void Introducao()
     {
-
-        static List<string> nomesDosConvidados = new List<string>();
-        static List<int> telefoneDosConvidados = new List<int>();
-        static void Main(string[] args) {   
-            var nomeDoUsuario = Introdução();            
-            Menu(nomeDoUsuario);                                
-        }
-
-        static string Introdução() {  
-            Console.WriteLine("Bem vindo ao realizador de festa!");
-            Console.WriteLine("Primeiramente informe seu nome!");
-            string nomeDoUsuario = Console.ReadLine();
-            Console.Clear(); 
-
-            return nomeDoUsuario;           
-        }
-
-        static void Menu(string nomeDoUsuario) {     
-                                                       
-            Console.WriteLine($"Olá {nomeDoUsuario}, Selecione uma das opções para continuar");
-            Console.WriteLine("1 - Adicionar Convidado.");
-            Console.WriteLine("2 - Remover Convidado.");
-            Console.WriteLine("3 - Editar Convidado.");
-            Console.WriteLine("4 - Listar Convidados pré-cadastrados.");
-            Console.WriteLine("5 - Acessar informações de seus convidado.");
-            Console.WriteLine("6 - Fechar o Programa.");
-            int MenuListaDeConvidados = Convert.ToInt32(Console.ReadLine()); 
-            Console.Clear(); 
-                                         
-                    switch (MenuListaDeConvidados) {
-                case 1:
-                    AdicionarConvidados(nomesDosConvidados, telefoneDosConvidados);                                          
-                    break;
-
-                case 2:
-                    RemoverConvidados(nomesDosConvidados);                                     
-                    break;
-
-                case 3:
-                    EditarConvidados(nomesDosConvidados, telefoneDosConvidados);
-                    break;
-
-                case 4:
-                    ListarConvidados(nomesDosConvidados);                    
-                    break;
-
-                case 5:
-                    AcessarInfoDosConvidados(nomesDosConvidados, telefoneDosConvidados);
-                    break;
-
-                case 6:
-                    FinalizarPrograma();
-                    break; 
-
-                default:
-                    Console.Write("Você digitou nenhum dos parãmetros corretamente.");
-                    Console.ReadKey();
-                    break;                               
-            }   
-            
-            Menu(nomeDoUsuario);                               
-        }
-        
-        static void AdicionarConvidados(List<string> nomesDosConvidados, List<int> telefoneDosConvidados) {
-            var ContinuarBloco = "s";               
-                                   
-            while (ContinuarBloco == "s" || ContinuarBloco == "S") {
-                Console.WriteLine("Informe o nome de seus convidados a serem adicionados na lista da festa.");
-                nomesDosConvidados.Add(Console.ReadLine());
-                        
-                Console.WriteLine("O telefone do seu usuário (sem ponto ou hífen) - EX: 900000000");
-                telefoneDosConvidados.Add(Convert.ToInt32(Console.ReadLine()));                        
-
-                Console.WriteLine("Deseja adicionar mais algum convidado? (s/N)");
-                ContinuarBloco = Console.ReadLine();          
-            }
-
-            Console.Clear();                                    
-        }
-
-        static void RemoverConvidados(List<string> nomesDosConvidados) {
-            var ContinuarBloco = "s";
-            
-            while (ContinuarBloco == "s" || ContinuarBloco == "S") {
-            Console.WriteLine("Informe o nome de um convidado a ser retirado da lista de sua festa!");
-            var ConvidadoASerRemovido = Console.ReadLine();             
-            
-            var PosicaoConvidado = nomesDosConvidados.FindIndex(nomeConvidado => nomeConvidado == ConvidadoASerRemovido);
-            nomesDosConvidados.RemoveAt(PosicaoConvidado);
-            telefoneDosConvidados.RemoveAt(PosicaoConvidado);
-
-            Console.WriteLine("Deseja remover mais algum convidado? (s/N)");
-            ContinuarBloco = Console.ReadLine();
-            }
-
-                Console.Clear();
-
-        }
-        
-        static void EditarConvidados(List<string> nomesDosConvidados, List<int> telefoneDosConvidados) {
-            var ContinuarBloco = "s";
-            
-            while (ContinuarBloco == "s" || ContinuarBloco == "S") {
-                Console.WriteLine("Informe o nome do convidado a ser editado!");
-                var ConvidadoASerEditado = Console.ReadLine();
-
-                var PosicaoConvidado = nomesDosConvidados.FindIndex(nomeConvidado => nomeConvidado == ConvidadoASerEditado);
-
-                Console.WriteLine("Você deseja editar o nome ou telefone?"); 
-                var EdicaoDoConvidado = Console.ReadLine();
-
-                if (EdicaoDoConvidado == "Nome" || EdicaoDoConvidado == "nome") {
-                    Console.WriteLine("Digite o novo nome!");
-                    var NovoNome = Console.ReadLine();
-                    nomesDosConvidados[PosicaoConvidado] = NovoNome;                 
-
-                } if (EdicaoDoConvidado == "Telefone" || EdicaoDoConvidado == "telefone") {
-                    Console.WriteLine("Digite o novo telefone!");
-                    var NovoTelefone = Console.ReadLine();
-                    telefoneDosConvidados[PosicaoConvidado] = Convert.ToInt32(NovoTelefone);
-                }
-
-                Console.WriteLine("Deseja editar mais algum convidado? (s/N)");
-                ContinuarBloco = Console.ReadLine();
-            }
-
-            Console.Clear();
-
-        }   
-
-        static void ListarConvidados(List<string> nomesDosConvidados) {
-            Console.WriteLine("Essas são as pessoas convidadas para a sua festa: ");
-            foreach(string nomeDoConvidado in nomesDosConvidados)  {
-               Console.WriteLine($"{nomeDoConvidado}");                
-            };
-            Console.WriteLine("Digite qualquer tecla para continuar");
-            Console.ReadKey();
-            Console.Clear();
-        }
-        
-        static void AcessarInfoDosConvidados(List<string> nomesDosConvidados, List<int> telefoneDosConvidados) {
-            Console.WriteLine("Essas são as pessoas convidadas para a sua festa: ");
-            foreach(string nomeDoConvidado in nomesDosConvidados) {
-                Console.WriteLine($"{nomeDoConvidado}");
-            }
-                        
-            var ContinuarBloco = "s";
-            
-            while (ContinuarBloco == "s" || ContinuarBloco == "S") {
-                Console.WriteLine("Qual convidado você deseja acessar a informação do telefone?");
-                var InformacaoDoConvidado = Console.ReadLine();
-
-                var PosicaoConvidado = nomesDosConvidados.FindIndex(nomeConvidado => nomeConvidado == InformacaoDoConvidado);
-                Console.WriteLine($"O telefone do(a) {nomesDosConvidados[PosicaoConvidado]} é {telefoneDosConvidados[PosicaoConvidado]}");
-                
-                Console.WriteLine("Deseja verificar mais algum convidado? (s/N)");
-                ContinuarBloco = Console.ReadLine();                
-            }
-
-            Console.Clear();
-
-        }
-
-        static void FinalizarPrograma() {
-            Console.WriteLine("Aperte qualquer tecla para finalizar o seu programa");
-            Console.ReadKey();
-            Environment.Exit(0);
-        }     
+      Console.WriteLine("Olá, seja bem vindo ao Exercício número 1!");
+      Console.WriteLine("Primeiramente, digite seu nome completo.");
+      var nomeUsuario = Console.ReadLine();
+      var nome = nomeUsuario;
     }
+
+    static void Menu()
+    {
+      Console.WriteLine($"Olá {nomeUsuario}, digite agora as informações solicitadas.");
+      Console.WriteLine("Primeiro, digite a data do seu nascimento no formato MM/DD/AAAA. (EX: 05/20/1997)");
+      var dataNascimento = DateTime.Parse(Console.ReadLine());
+      Console.WriteLine("Agora digite a sua altura. (EX: 1.63)");
+      var altura = Console.ReadLine();
+      
+      CalcularIdade();
+    }
+    
+    static void CalcularIdade()
+    {
+      var idade = DateTime.Today.Year - dataNascimento.Year; 
+    }
+
+    static void ImprimirDados()
+    { 
+      Console.WriteLine($"Seu nome completo é {Pessoa.Nome}, você nasceu em {Pessoa.DataNascimento} ({idade} anos), e possui {altura} de altura!");
+      Console.WriteLine("Clique para finalizar o programa.");
+      Console.ReadKey();
+    }
+  }
 }
